a536bcee0d36589d1e3cea019604960cf9303e0a
Tom Satterthwaite <thomassatterthwaite@dhcp-206-87-219-173.ubcsecure.wireless.ubc.ca>
2015-05-11 15:25:13 -0700

    Chronology chart added

--- graphtable.js
+++ graphtable.js
@@ -1,7 +1,7 @@
 
 function drawAllTheYearsChart() {
 				
-			var data = new google.visualization.DataTable;
+			var data = new google.visualization.DataTable();
 			data.addColumn('string', 'Month');
 			data.addColumn('number', 2004);
 			data.addColumn('number', 2005);
@@ -30,7 +30,7 @@ function drawAllTheYearsChart() {
 				
 			]);
 			
-			var options = {'title':'Passenger Volume for easyJet 2004-2014',
+			var options = {'title':'Passenger Volume for easyJet each year (2004-2014)',
                      'width':1000,
                      'height':500};
 
@@ -39,9 +39,167 @@ function drawAllTheYearsChart() {
 			
 			
 			
-			var chart = new google.charts.Line(document.getElementById('All the years'));
+			var chart = new google.charts.Line(document.getElementById('All_the_years_chart'));
 			chart.draw(data, options);
 			
 			
 			}
+
+function drawChronologyChart(){
 	
+	var data = new google.visualization.DataTable();
+	data.addColumn('date', "year")
+	data.addColumn('number', "volume")	
+	data.addRows([
+		[new Date(2004, 0), 1683699],
+		[new Date(2004, 1), 1864970],
+		[new Date(2004, 2), 1996790],
+		[new Date(2004, 3), 1947675], 
+		[new Date(2004, 4), 2092709],
+		[new Date(2004, 5), 2241252],
+		[new Date(2004, 6), 2413367],
+		[new Date(2004, 7), 2459735],
+		[new Date(2004, 8), 2355324], 
+		[new Date(2004, 9), 2405073], 
+		[new Date(2004, 10), 2120948], 
+		[new Date(2004, 11), 2134787],
+		[new Date(2005, 0), 2083852],
+		[new Date(2005, 1), 2168985],
+		[new Date(2005, 2), 2573070],
+		[new Date(2005, 3), 2438194],
+		[new Date(2005, 4), 2551619],
+		[new Date(2005, 5), 2586889],
+		[new Date(2005, 6), 2847598],
+		[new Date(2005, 7), 2903404],
+		[new Date(2005, 8), 2743221], 
+		[new Date(2005, 9), 2734931], 
+		[new Date(2005 ,10), 2297895],
+		[new Date(2005, 11), 2372333],
+		[new Date(2006, 0), 2316749],
+		[new Date(2006, 1), 2373706],
+		[new Date(2006, 2), 2754705],
+		[new Date(2006, 3), 2848065],
+		[new Date(2006, 4), 2939361], 
+		[new Date(2006, 5), 2990169],
+		[new Date(2006, 6), 3168012],
+		[new Date(2006, 7), 3146216],
+		[new Date(2006, 8), 3011145],
+		[new Date(2006, 9), 2935355],
+		[new Date(2006, 10), 2554143],
+		[new Date(2006, 11), 2638279],
+		[new Date(2007, 0), 2573451],
+		[new Date(2007, 1), 2646775],
+		[new Date(2007, 2), 3094588],
+		[new Date(2007, 3), 3133725],
+		[new Date(2007, 4), 3345465],
+		[new Date(2007, 5), 3440639],
+		[new Date(2007, 6), 3723004],
+		[new Date(2007, 7), 3706354],
+		[new Date(2007, 8), 3438261],
+		[new Date(2007, 9), 3344916],
+		[new Date(2007, 10), 2888133],
+		[new Date(2007, 11), 2899349],
+		[new Date(2008, 0), 2761583],
+		[new Date(2008, 1), 3240767],
+		[new Date(2008, 2), 3730134],
+		[new Date(2008, 3), 3553023],
+		[new Date(2008, 4), 3877960],
+		[new Date(2008, 5), 4112951],
+		[new Date(2008, 6), 4467268],
+		[new Date(2008, 7), 4585739],
+		[new Date(2008, 8), 4197643],
+		[new Date(2008, 9), 3959194],
+		[new Date(2008, 10), 2985826],
+		[new Date(2008, 11), 3111388],
+		[new Date(2009, 0),2839617],
+		[new Date(2009, 1), 3018910],
+		[new Date(2009, 2), 3494312],
+		[new Date(2009, 3), 3776582],
+		[new Date(2009, 4), 3948416],
+		[new Date(2009, 5), 4146609],
+		[new Date(2009, 6), 4661068],
+		[new Date(2009, 7), 4800336],
+		[new Date(2009, 8), 4422021],
+		[new Date(2009, 9), 4219096],
+		[new Date(2009, 10), 3351187],
+		[new Date(2009, 11), 3399305],
+		[new Date(2010, 0), 3142629],
+		[new Date(2010, 1), 3390523],
+		[new Date(2010, 2), 3964399],
+		[new Date(2010, 3), 3490599],
+		[new Date(2010, 4), 4258675],
+		[new Date(2010, 5), 4537959],
+		[new Date(2010, 6), 5021838],
+		[new Date(2010, 7), 5203165],
+		[new Date(2010, 8), 4774991],
+		[new Date(2010, 9), 4582632],
+		[new Date(2010, 10), 3694514],
+		[new Date(2010, 11), 3657186],
+		[new Date(2011, 0), 3743593],
+		[new Date(2011, 1), 3833667],
+		[new Date(2011, 2), 4435624],
+		[new Date(2011, 3), 4717402],
+		[new Date(2011, 4), 4739436],
+		[new Date(2011, 5), 4952305],
+		[new Date(2011, 6), 5427112],
+		[new Date(2011, 7), 5543961],
+		[new Date(2011, 8), 5181839],
+		[new Date(2011, 9), 4939904],
+		[new Date(2011, 10), 3821355],
+		[new Date(2011, 11), 4135562],
+		[new Date(2012, 0), 3728514],
+		[new Date(2012, 1), 3976741],
+		[new Date(2012, 2), 4629241],
+		[new Date(2012, 3), 5124597],
+		[new Date(2012, 4), 5423726],
+		[new Date(2012, 5), 5434763],
+		[new Date(2012, 6), 5860272],
+		[new Date(2012, 7), 5873948],
+		[new Date(2012, 8), 5451217],
+		[new Date(2012, 9), 5245201],
+		[new Date(2012, 10), 4116576],
+		[new Date(2012, 11), 4339836],
+		[new Date(2013, 0), 3878640],
+		[new Date(2013, 1), 4112186],
+		[new Date(2013, 2), 4872934],
+		[new Date(2013, 3), 5253610],
+		[new Date(2013, 4), 5609351],
+		[new Date(2013, 5), 5537275],
+		[new Date(2013, 6), 5976704],
+		[new Date(2013, 7), 6101344],
+		[new Date(2013, 8), 5714152],
+		[new Date(2013, 9), 5530091],
+		[new Date(2013, 10), 4255978],
+		[new Date(2013, 11), 4490538],
+		[new Date(2014, 0), 4021678],
+		[new Date(2014, 1), 4232325],
+		[new Date(2014, 2), 5107676],
+		[new Date(2014, 3), 5787833],
+		[new Date(2014, 4), 6054249],
+		[new Date(2014, 5), 6098364],
+		[new Date(2014, 6), 6434284],
+		[new Date(2014, 7), 6612075],
+		[new Date(2014, 8), 6143974],
+		[new Date(2014, 9), 5835145],
+		[new Date(2014, 10), 4537369],
+		[new Date(2014, 11), 4836276]
+		
+	]);
+	
+	var options = {'title':'Total Passenger Volume for easyJet 2004-2014',
+                     'width':1000,
+                     'height':500};
+
+			
+			
+			
+			
+			
+			var chart = new google.charts.Line(document.getElementById('Chronology_chart'));
+			chart.draw(data, options);
+			
+
+	
+	
+}
+			
