fa7a488c194cf5138192c1bd8365bea73b5c2fc8
Tom Satterthwaite <thomassatterthwaite@Thomass-MacBook-Pro.local>
2015-05-11 13:56:25 -0700

    Making graphs for dayze.

--- graphtable.js
+++ graphtable.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-var data = [ { x: "Jan 2010", y: 40 }, { x: 1, y: 49 }, { x: 2, y: 17 }, { x: 3, y: 42 } ];
+var data = [ { x: 0 , y: 40 }, { x: 1, y: 49 }, { x: 2, y: 17 }, { x: 3, y: 42 } ];
 
 var graph = new Rickshaw.Graph( {
         element: document.querySelector("#chart"),
@@ -7,9 +7,13 @@ var graph = new Rickshaw.Graph( {
         height: 250,
         series: [ {
                 color: 'steelblue',
-                data: data
+                data: data,
+                renderer: 'line',
         } ]
-} );
+});
+
+var axes = new Rickshaw.Graph.Axis.Time( {graph: graph});
+                
 
 graph.render();
 });
