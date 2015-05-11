9718e651b47aa4779835531ff182d9bd9ee9c9b0
Tom Satterthwaite <thomassatterthwaite@dhcp-206-87-219-173.ubcsecure.wireless.ubc.ca>
2015-05-11 16:34:29 -0700

    Upto YOY

--- graphtable.js
+++ graphtable.js
@@ -1,56 +1,5 @@
-
-function drawAllTheYearsChart() {
-				
-			var data = new google.visualization.DataTable();
-			data.addColumn('string', 'Month');
-			data.addColumn('number', 2004);
-			data.addColumn('number', 2005);
-			data.addColumn('number', 2006);
-			data.addColumn('number', 2007);
-			data.addColumn('number', 2008);
-			data.addColumn('number', 2009);
-			data.addColumn('number', 2010);
-			data.addColumn('number', 2011);
-			data.addColumn('number', 2012);
-			data.addColumn('number', 2013);
-			data.addColumn('number', 2014);
-			data.addRows([
-				['Jan', 1683699, 2083852, 2316749, 2573451, 2761583, 2839617, 3142629, 3743593, 3728514, 3878640, 4021678 ],
-				['Feb',  1864970, 	 2168985, 	 2373706, 	 2646775, 	 3240767, 	 3018910, 	 3390523, 	 3833667, 	 3976741, 	 4112186, 	 4232325 ],
-				['Mar',  1996790, 	 2573070, 	 2754705, 	 3094588, 	 3730134, 	 3494312, 	 3964399, 	 4435624, 	 4629241, 	 4872934, 	 5107676 ],
-				['Apr',  1947675, 	 2438194, 	 2848065, 	 3133725, 	 3553023, 	 3776582, 	 3490599, 	 4717402, 	 5124597, 	 5253610, 	 5787833 ],
-				['May',  2092709,	2551619,	2939361,	3345465,	3877960,	3948416,	4258675,	4739436,	5423726,	5609351,	6054249],
-				['Jun', 2241252,	2586889,	2990169,	3440639,	4112951,	4146609,	4537959,	4952305,	5434763,	5537275,	6098364],
-				['Jul', 2413367,	2847598,	3168012,	3723004,	4467268,	4661068,	5021838,	5427112,	5860272,	5976704,	6434284],
-				['Aug', 2459735,	2903404,	3146216,	3706354,	4585739,	4800336,	5203165,	5543961,	5873948,	6101344,	6612075],
-				['Sep', 2355324,	2743221,    3011145,	3438261,	4197643, 4422021,	4774991,	5181839,	5451217,	5714152,	6143974],
-				['Oct', 2405073,	2734931,	2935355,	3344916,	3959194,	4219096,	4582632,	4939904,	5245201,	5530091,	5835145],
-				['Nov', 2120948,	2297895,	2554143,	2888133,	2985826,	3351187,	3694514,	3821355,	4116576,	4255978,	4537369],
-				['Dec', 2134787,	2372333,	2638279,	2899349,	3111388,	3399305,	3657186,	4135562,	4339836,	4490538,	4836276]
-				
-			]);
-			
-			var options = {'title':'Passenger Volume for easyJet each year (2004-2014)',
-                     'width':1000,
-                     'height':500};
-
-			
-			
-			
-			
-			
-			var chart = new google.charts.Line(document.getElementById('All_the_years_chart'));
-			chart.draw(data, options);
-			
-			
-			}
-
-function drawChronologyChart(){
-	
-	var data = new google.visualization.DataTable();
-	data.addColumn('date', "year")
-	data.addColumn('number', "volume")	
-	data.addRows([
+var data_set = new Array();
+	data_set = [
 		[new Date(2004, 0), 1683699],
 		[new Date(2004, 1), 1864970],
 		[new Date(2004, 2), 1996790],
@@ -184,9 +133,39 @@ function drawChronologyChart(){
 		[new Date(2014, 10), 4537369],
 		[new Date(2014, 11), 4836276]
 		
-	]);
-	
-	var options = {'title':'Total Passenger Volume for easyJet 2004-2014',
+	];
+function drawAllTheYearsChart() {
+				
+			var data = new google.visualization.DataTable();
+			data.addColumn('string', 'Month');
+			data.addColumn('number', 2004);
+			data.addColumn('number', 2005);
+			data.addColumn('number', 2006);
+			data.addColumn('number', 2007);
+			data.addColumn('number', 2008);
+			data.addColumn('number', 2009);
+			data.addColumn('number', 2010);
+			data.addColumn('number', 2011);
+			data.addColumn('number', 2012);
+			data.addColumn('number', 2013);
+			data.addColumn('number', 2014);
+			data.addRows([
+				['Jan', 1683699, 2083852, 2316749, 2573451, 2761583, 2839617, 3142629, 3743593, 3728514, 3878640, 4021678 ],
+				['Feb',  1864970, 	 2168985, 	 2373706, 	 2646775, 	 3240767, 	 3018910, 	 3390523, 	 3833667, 	 3976741, 	 4112186, 	 4232325 ],
+				['Mar',  1996790, 	 2573070, 	 2754705, 	 3094588, 	 3730134, 	 3494312, 	 3964399, 	 4435624, 	 4629241, 	 4872934, 	 5107676 ],
+				['Apr',  1947675, 	 2438194, 	 2848065, 	 3133725, 	 3553023, 	 3776582, 	 3490599, 	 4717402, 	 5124597, 	 5253610, 	 5787833 ],
+				['May',  2092709,	2551619,	2939361,	3345465,	3877960,	3948416,	4258675,	4739436,	5423726,	5609351,	6054249],
+				['Jun', 2241252,	2586889,	2990169,	3440639,	4112951,	4146609,	4537959,	4952305,	5434763,	5537275,	6098364],
+				['Jul', 2413367,	2847598,	3168012,	3723004,	4467268,	4661068,	5021838,	5427112,	5860272,	5976704,	6434284],
+				['Aug', 2459735,	2903404,	3146216,	3706354,	4585739,	4800336,	5203165,	5543961,	5873948,	6101344,	6612075],
+				['Sep', 2355324,	2743221,    3011145,	3438261,	4197643, 4422021,	4774991,	5181839,	5451217,	5714152,	6143974],
+				['Oct', 2405073,	2734931,	2935355,	3344916,	3959194,	4219096,	4582632,	4939904,	5245201,	5530091,	5835145],
+				['Nov', 2120948,	2297895,	2554143,	2888133,	2985826,	3351187,	3694514,	3821355,	4116576,	4255978,	4537369],
+				['Dec', 2134787,	2372333,	2638279,	2899349,	3111388,	3399305,	3657186,	4135562,	4339836,	4490538,	4836276]
+				
+			]);
+			
+			var options = {'title':'Passenger Volume per year (2004-2014)',
                      'width':1000,
                      'height':500};
 
@@ -195,6 +174,24 @@ function drawChronologyChart(){
 			
 			
 			
+			var chart = new google.charts.Line(document.getElementById('All_the_years_chart'));
+			chart.draw(data, options);
+			
+			
+			}
+
+function drawChronologyChart(){
+	
+	var data = new google.visualization.DataTable();
+	data.addColumn('date', "Year")
+	data.addColumn('number', "Total Passenger Volume")	
+	data.addRows(data_set);
+	
+	var options = {'title':'Total Passenger Volume 2004-2014',
+                     'width':1000,
+                     'height':500};
+
+			
 			var chart = new google.charts.Line(document.getElementById('Chronology_chart'));
 			chart.draw(data, options);
 			
@@ -202,4 +199,49 @@ function drawChronologyChart(){
 	
 	
 }
+
+function drawIndexChart(){
+	var data = new google.visualization.DataTable();
+	data.addColumn('date', "Year");
+	data.addColumn('number', 'Index trend from Janury 2004');
+	var row_data = new Array();
+	var first_term = data_set[0][1];
+	console.log(first_term);
+	for (i=0; i<data_set.length; i++){
+		row_data[row_data.length] = [data_set[i][0], (data_set[i][1]/first_term)*100];
+	}
+	data.addRows(row_data);
+	
+	var options = {'title':'Total Passenger Index 2004-2014',
+                     'width':1000,
+                     'height':500};
+
+			var chart = new google.charts.Line(document.getElementById('Index_chart'));
+			chart.draw(data, options);
+}
+
+function drawYOYChart(){
+	
+	var data = new google.visualization.DataTable();
+	data.addColumn('date', "Year");
+	data.addColumn('number', 'YoY trend from Janury 2004');
+	var row_data = new Array();
+	
+	for (i=0, j=12; i<data_set.length-12, j<data_set.length; i++, j++){
+		row_data[row_data.length] = [data_set[j][0], (data_set[j][1]/data_set[i][1])-1];
+		
+	}
+	
+	data.addRows(row_data);
+	
+	var options = {'title':'Passenger Year on Year 2004-2014',
+                     'width':1000,
+                     'height':500};
+
+			var chart = new google.charts.Line(document.getElementById('YoY_chart'));
+			chart.draw(data, options);
+}
+
+
+	
 			
